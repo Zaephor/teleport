0c0167da2a2c9e2728ef3d9543a6466b5c89224a
Zaephor <Zaephor@users.noreply.github.com>
2019-08-15 20:55:34 -0700

    Being nice to the CI people, skipping teleport <2

--- generate_tags.sh
+++ generate_tags.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-for z in $(git ls-remote --tags https://github.com/gravitational/teleport.git | awk -F '/' '!/{}/ && /v/{print $NF}' | grep -v '\(alpha\|beta\|rc\|v0.\)'); do
-	if ! grep -q "^${z}$" VERSIONS; then
-		echo ${z} >> VERSIONS
-		git add VERSIONS
-		git commit -m "TP: ${z}"
-		git tag ${z}
-		git push -u origin master
-		git push --tags
-		sleep 10m
+for z in $(git ls-remote --tags https://github.com/gravitational/teleport.git | awk -F '/' '!/{}/ && /v/{print $NF}' | grep -v '\(alpha\|beta\|rc\|v0.\)' | sort -V); do
+	MAJOR=$(echo "${z}" | awk -F '[.v]' '/v/{print $2}')
+	if [[ -n "${MAJOR}" && ${MAJOR} -ge 3 ]]; then
+		if ! grep -q "^${z}$" VERSIONS; then
+			echo ${z} >> VERSIONS
+			git add VERSIONS
+			git commit -m "TP: ${z}"
+			git tag ${z}
+			git push -u origin master
+			git push --tags
+			sleep 10m
+		fi
 	fi
 done
