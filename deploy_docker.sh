d3a9eb93361309c7e7ae72d66267abcbb108a23d
Zaephor <Zaephor@users.noreply.github.com>
2019-08-16 17:53:10 -0700

    Adjustments for disabling SSL when behind a proxy

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 if [[ ${ALT_BRANCH} == ${TRAVIS_TAG} ]]; then
 	docker tag ${DOCKER_TAG}:${ARCH_LABEL}-${REMOTE_BRANCH} ${DOCKER_TAG}:${ARCH_LABEL}
-	if [[ ${ARCH_LABEL} == "amd64" ]]; then
+	if [[ "${ARCH_LABEL}" == "amd64" ]]; then
 		docker tag ${DOCKER_TAG}:${ARCH_LABEL}-${REMOTE_BRANCH} ${DOCKER_TAG}:latest
 	fi
-	SUBTAG=${REMOTE_BRANCH}
-	while [[ -n "${SUBTAG//[^.]}" ]]; do
-		docker tag ${DOCKER_TAG}:${ARCH_LABEL}-${REMOTE_BRANCH} ${DOCKER_TAG}:${ARCH_LABEL}-${SUBTAG%.**}
-		SUBTAG=${SUBTAG%.**}
-	done
 fi
+SUBTAG=${REMOTE_BRANCH}
+while [[ -n "${SUBTAG//[^.]}" ]]; do
+	LATEST_MATCH=$(awk "/${SUBTAG%.**}/ {a=\$0} END{print a}" "${TRAVIS_BUILD_DIR}/VERSIONS")
+	if [[ "${LATEST_MATCH}" == "${TRAVIS_TAG}" ]]; then
+		docker tag ${DOCKER_TAG}:${ARCH_LABEL}-${REMOTE_BRANCH} ${DOCKER_TAG}:${ARCH_LABEL}-${SUBTAG%.**}
+	fi
+	SUBTAG=${SUBTAG%.**}
+done
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin &>/dev/null
 docker push ${DOCKER_TAG}
