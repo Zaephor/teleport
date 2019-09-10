67fde3792edef3a9625982409173a775fecab2bf
Zaephor <Zaephor@users.noreply.github.com>
2019-09-09 19:41:42 -0700

    ci: checking and bumping docker

--- build-script.sh
+++ build-script.sh
@@ -4,3 +4,18 @@ echo "0: $0"
 echo "@: $@"
 echo "ARCH_LABEL: ${ARCH_LABEL}"
 echo "BUILD_GOOS: ${BUILD_GOOS}"
+
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+
+docker version
+
+STAGE=$(echo "${1#*-}" | tr '[a-z]' '[A-Z]')
+case ${STAGE} in
+	"binary")
+		echo ${STAGE}
+		;;
+	"docker")
+		echo ${STAGE}
+		;;
+esac
