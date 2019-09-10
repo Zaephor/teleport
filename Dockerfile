db97719d112556b83407af14377a2bc6094c190b
Zaephor <Zaephor@users.noreply.github.com>
2019-09-09 20:51:05 -0700

    Got multi-stage builds kinda working

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:1.9.7-alpine3.8 AS build
+ARG REMOTE_BRANCH=master
+RUN echo "=== ${REMOTE_BRANCH}" && \
+    apk add --no-cache \
+      git make gcc musl-dev zip tar && \
+    git clone -q --depth=1 --branch=${REMOTE_BRANCH} https://github.com/gravitational/teleport.git /go/src/github.com/gravitational/teleport && \
+    cd /go/src/github.com/gravitational/teleport && git checkout -qf ${REMOTE_BRANCH} && \
+    make release && \
+    mkdir temp/ && tar -xvf teleport-*.tar.gz -C ./temp --strip-components=1 teleport/teleport teleport/tctl teleport/tsh
+#    apk del git make
+
+FROM lsiobase/alpine:3.10
+EXPOSE 3022 3023 3024 3025 3026 3080
+VOLUME /config
+#COPY temp/* /usr/sbin/
+COPY s6/ /
+COPY --from=build /go/src/github.com/gravitational/teleport/temp/* /usr/sbin/
--- build-script.sh
+++ build-script.sh
@@ -5,17 +5,47 @@ echo "@: $@"
 echo "ARCH_LABEL: ${ARCH_LABEL}"
 echo "BUILD_GOOS: ${BUILD_GOOS}"
 
-echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
-
 docker version
 
-STAGE=$(echo "${1#*-}" | tr '[a-z]' '[A-Z]')
+STAGE=$(echo "${1#*-}" | tr '[A-Z]' '[a-z]')
 case ${STAGE} in
 	"binary")
 		echo ${STAGE}
+		
 		;;
 	"docker")
 		echo ${STAGE}
+#		docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}" &> /dev/null
+
+		docker run --rm --privileged multiarch/qemu-user-static:register
+		docker run --privileged linuxkit/binfmt:v0.6
+
+		docker buildx create --name teleport
+		docker buildx use teleport
+		docker buildx ls
+
+		DOCKER_TAGS=()
+		ALT_BRANCH=$(tail -n1 ${TRAVIS_BUILD_DIR}/VERSIONS) # Detect last version in VERSIONS file
+		REMOTE_BRANCH=${TRAVIS_TAG:-${ALT_BRANCH}} # Detect used tag
+		if [[ -n "${ALT_BRANCH}" && -n "${TRAVIS_TAG}" && "${ALT_BRANCH}" == "${TRAVIS_TAG}" ]]; then
+			DOCKER_TAGS+=( "-t" "${DOCKER_TAG}:latest" )
+		fi
+		DOCKER_TAGS+=( "-t" "${DOCKER_TAG}:${REMOTE_BRANCH}" )
+
+		SUBTAG=${REMOTE_BRANCH}
+		while [[ -n "${SUBTAG//[^.]}" ]]; do
+			LATEST_MATCH=$(awk "/${SUBTAG%.**}/ {a=\$0} END{print a}" "${TRAVIS_BUILD_DIR}/VERSIONS")
+			if [[ "${LATEST_MATCH}" == "${TRAVIS_TAG}" ]]; then
+				DOCKER_TAGS+=( "-t" "${DOCKER_TAG}:${SUBTAG%.**}" )
+				docker tag ${DOCKER_TAG}:${ARCH_LABEL}-${REMOTE_BRANCH} ${DOCKER_TAG}:${ARCH_LABEL}-${SUBTAG%.**}
+			fi
+			SUBTAG=${SUBTAG%.**}
+		done
+
+		docker buildx build --platform "${PLATFORMS}" --build-arg REMOTE_BRANCH=${REMOTE_BRANCH} ${DOCKER_TAGS[@]} --push -f "Dockerfile" .
+
+		docker buildx imagetools inspect "${DOCKER_TAG}:${REMOTE_BRANCH}"
+		docker manifest inspect "${DOCKER_TAG}:${REMOTE_BRANCH}"
+
 		;;
 esac
