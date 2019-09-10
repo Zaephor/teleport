8ef74d6d0d3795e0b5b28b9b87bd1e5bb49f995a
Zaephor <Zaephor@users.noreply.github.com>
2019-09-09 19:21:16 -0700

    ci: idea2

--- build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-echo "0: $0"
-echo "ARCH_LABEL: ${ARCH_LABEL}"
-echo "BUILD_GOOS: ${BUILD_GOOS}"
--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 echo "0: $0"
+echo "@: $@"
 echo "ARCH_LABEL: ${ARCH_LABEL}"
 echo "BUILD_GOOS: ${BUILD_GOOS}"
