3a280b44668e643291e4a6df15ad757e1e5479a9
Zaephor <Zaephor@users.noreply.github.com>
2021-02-11 23:46:30 -0800

    ci: add logging to package script

--- .ci/package.sh
+++ .ci/package.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 OPWD=$(pwd)
+
+## Func
+function _log {
+        echo "==== ${@}"
+}
+
+
 if [[ ! -d /tmp/tars ]]; then
+	_log "Create temporary folder"
 	mkdir -p /tmp/tars
 fi
+_log "Copying artifact tars to temporary folder"
 cp artifacts/teleport-*.tar.gz /tmp/tars
 for pkg in 'deb'; do
+	_log "Building fpm container for ${pkg}"
 	docker build -t "fpm:${pkg}" -f Dockerfile.${pkg}.fpm .
 	for TAR_FILE in $(ls -1 /tmp/tars | grep linux); do
+		_log "Processing ${TAR_FILE}"
 		TAR_ARCH=$(echo "${TAR_FILE}" | awk -F '[-.]' '{print $(NF-2)}')
+		_log "${TAR_FILE} - Identified ${TAR_ARCH}"
+		_log "${TAR_FILE} - Creating more temp folders"
 		mkdir -p /tmp/tp-${pkg}-${TAR_ARCH}/usr/local/bin /tmp/tp-${pkg}-${TAR_ARCH}/etc/systemd/system /tmp/tp-${pkg}-${TAR_ARCH}/etc/init
+		_log "${TAR_FILE} - Copying assets into position"
 		cp systemd-teleport.service /tmp/tp-${pkg}-${TAR_ARCH}/etc/systemd/system/teleport.service
 		cp upstart-teleport.conf /tmp/tp-${pkg}-${TAR_ARCH}/etc/init/teleport.conf
 		tar -xvf /tmp/tars/${TAR_FILE} -C /tmp/tp-${pkg}-${TAR_ARCH}/usr/local/bin --strip-components=1 teleport/teleport teleport/tctl teleport/tsh teleport/VERSION
+		_log "${TAR_FILE} - Identifying package version"
 		BUILD_TARGET=$(cat /tmp/tp-${pkg}-${TAR_ARCH}/usr/local/bin/VERSION)
 		rm /tmp/tp-${pkg}-${TAR_ARCH}/usr/local/bin/VERSION
-		docker run --rm -it -v "/tmp/tp-${pkg}-${TAR_ARCH}:/tmp/fpm" -w "/tmp/fpm" \
+		_log "${TAR_FILE} - Launching fpm container to build desired ${pkg} file"
+		docker run --rm -t $(tty &>/dev/null && echo "-i") -v "/tmp/tp-${pkg}-${TAR_ARCH}:/tmp/fpm" -w "/tmp/fpm" \
 			"fpm:${pkg}" \
 				-n 'teleport' \
 				-v ${BUILD_TARGET/v/} \
@@ -28,6 +44,7 @@ for pkg in 'deb'; do
 				./usr/local/bin/=/usr/local/bin \
 				./etc/=/etc
 #				./teleport.dir/usr/share/=/usr/share \
+		_log "${TAR_FILE} - Copying resulting ${pkg} file to primary artifacts directory"
 		cp /tmp/tp-${pkg}-${TAR_ARCH}/teleport_*.${pkg} ${OPWD}/artifacts/
 	done
 done
